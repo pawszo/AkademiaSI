4ae8b137ee7c91efa54fb48a259f0f2e22b124bd
pawszo <paulszoltysek@gmail.com>
2021-11-04 22:17:42 +0100

    Add IOutputWriter interface and service classes

---
+++ Lekcja4/Lekcja4/InterfacesAndStrategy/Interfaces/IOutputWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndStrategy.Interfaces
+{
+    public interface IOutputWriter
+    {
+        void UserInfo(string message);
+        void UserWarning(string message);
+        void UserError(string message);
+        void Debug(string message);
+
+    }
+}
